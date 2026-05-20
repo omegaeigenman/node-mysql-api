326116e2af29330738d441726208dc451f0da33e
lansiiy <lanceeigenmanomega@gmail.com>
2026-05-21 02:18:44 +0800

    dabolyo

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -4,7 +4,6 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
 
-// Use process.cwd() to find swagger.yaml from project root
 const swaggerDocument = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
